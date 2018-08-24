b1b221cf02dc5c597326a20ba663615f686a08e5
hahn-will <42660134+hahn-will@users.noreply.github.com>
2018-08-24 12:59:06 +0100

    Created the first javascript drawing

---
+++ Start.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("MainCanvas");
+var context = canvas.getContext("2d");
+context.font = "30px Arial";
+context.fillText(window.innerWidth + "X" + window.innerHeight,10,50);
