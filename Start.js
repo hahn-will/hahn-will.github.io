3eca1d89e9654e9d82ce941b8926b033de81fad8
hahn-will <42660134+hahn-will@users.noreply.github.com>
2018-08-24 17:04:26 +0100

    Added pdf of resume and tweaked individual parts of the website

--- Start.js
+++
@@ -1,5 +0,0 @@
-var canvas = document.getElementById("MainCanvas");
-var context = canvas.getContext("2d");
-context.font = "30px Arial";
-var height = window.innerHeight;
-var width = window.innerWidth;
